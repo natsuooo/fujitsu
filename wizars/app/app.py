476c3b9c6729854e3ffc9906ee4db663fc550f2f
Natsuooo <n17975775@gmail.com>
2021-02-17 01:44:06 +0000

    とりあえず完成で

--- wizars/app/app.py
+++ wizars/app/app.py
@@ -6,58 +6,39 @@ import random
 
 app = Flask(__name__)
 
-
+# 先生一覧画面
 @app.route("/")
 @app.route("/index")
-# @app.route("/teachers")
-def index():
+@app.route("/teachers")
+def teachers():
     teachers = Teacher.query.all()
-    return render_template("teachers.html", teachers=teachers)
+    return render_template("teachers.html", teachers=teachers) 
 
-@app.route("/request", methods=["POST"])
-def req():
-    if request.method == "POST":
-        teacher_id = request.form["teacher_id"]
-        lecture = Lecture(teacher_id, datetime.now())
-        db_session.add(lecture)
-        db_session.commit()
-    return redirect(url_for("mypage"))
+# 先生詳細画面
+@app.route("/teachers/<int:id>")
+def teacher(id):
+    reviews = Review.query.filter_by(teacher_id=id).all()
+    teacher = Teacher.query.filter_by(id=id).first()
+    return render_template("teacher.html", teacher=teacher, reviews=reviews)
 
+# マイページ
 @app.route("/mypage")
 def mypage():
-    # if request.method == "POST":
-    #     teacher_id = request.form["teacher_id"]
-    #     lecture = Lecture(teacher_id, datetime.now())
-    #     db_session.add(lecture)
-    #     db_session.commit()
-    # elif request.method == "POST":
-
     # 非効率．余裕があればリレーショナルDBへ．
     lectures = Lecture.query.all()
-    teachers = []
-    for lecture in lectures:
-        teacher = Teacher.query.filter_by(id=lecture.teacher_id).first()
-        teachers.append(teacher)
-    return render_template("mypage.html", teachers=teachers)
+    lecture = Lecture.query.first()
+    if lecture != None:
+        teachers = []
+        for lecture in lectures:
+            teacher = Teacher.query.filter_by(id=lecture.teacher_id).first()
+            teachers.append(teacher)
+        return render_template("mypage.html", teachers=teachers)
+    else:
+        return render_template("mypage.html")
     
-
-@app.route("/teachers/<int:id>", methods=["GET", "POST"])
-def teacher(id):
-    # if request.method == "POST":
-    #     teacher_id = request.form["teacher_id"]
-    #     lecture = Lecture(teacher_id, datetime.now())
-    #     db_session.add(lecture)
-    #     db_session.commit()
-
-    teacher = Teacher.query.filter_by(id=id).first()
-    return render_template("teacher.html", teacher=teacher)
-
-@app.route("/register")
+# 先生登録
+@app.route("/register", methods=["POST"])
 def register():
-    return render_template("register.html")
-
-@app.route("/teachers", methods=["GET", "POST"])
-def teachers():
     if request.method == "POST":
         name = request.form["name"]
         subject = request.form["subject"]
@@ -68,10 +49,29 @@ def teachers():
         teacher = Teacher(name, subject, price, time, text, image, datetime.now())
         db_session.add(teacher)
         db_session.commit()
+    return redirect(url_for("teachers"))
 
-    teachers = Teacher.query.all()
-    return render_template("teachers.html", teachers=teachers)    
+# リクエスト
+@app.route("/request", methods=["POST"])
+def req():
+    if request.method == "POST":
+        teacher_id = request.form["teacher_id"]
+        lecture = Lecture(teacher_id, datetime.now())
+        db_session.add(lecture)
+        db_session.commit()
+    return redirect(url_for("mypage"))  
 
+# レビュー投稿
+@app.route("/review", methods=["POST"])
+def review():
+    if request.method == "POST":
+        teacher_id = request.form["teacher_id"]
+        rating = request.form["rating"]
+        text = request.form["text"]
+        review = Review(teacher_id, rating, text, datetime.now())
+        db_session.add(review)
+        db_session.commit()
+    return redirect(url_for("mypage"))
 
 
 if __name__ == "__main__":
